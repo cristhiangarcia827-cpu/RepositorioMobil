cf780e709849dc15f3efb9ca72fb0ba8db284095
Cristhian Garcia <cristhiangarcia827@gmail.com>
2025-11-03 19:40:48 -0600

    Avance de Semana 3

--- MyBeautyApp/App.tsx
+++ MyBeautyApp/App.tsx
@@ -1,34 +1,11 @@
-import { StyleSheet, Text, View } from 'react-native';
-import { useState } from 'react';
-import CustomInput from './src/components/Customlnput';
+import { NavigationContainer } from "@react-navigation/native"
+import StackNavigator from "./src/navigation/StackNavigator"
 
-export default function App() {
-  const [email, setEmail] = useState('');
 
-  return (
-    <View style={styles.container}>
-      <CustomInput
-        value={email}
-        placeholder={'Correo'}
-        onChange={setEmail}
-      />
-    </View>
-  );
+export default function App(){
+  return(
+    <NavigationContainer>
+      <StackNavigator/>
+    </NavigationContainer>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    //habilita uso de flexbox para distribucion de espacio
-    flex: 1,
-    //distribucion en eje horizontal
-    alignItems: 'center',
-    //alineacion en eje vertical
-    justifyContent: 'center',
-    padding: 20,
-    backgroundColor: 'red',
-  },
-  content: {
-    color: '#fff',
-  }
-});
-
--- MyBeautyApp/src/components/CustomButton.tsx
+++ MyBeautyApp/src/components/CustomButton.tsx
@@ -5,30 +5,31 @@ type Props = {
     onPress: () => void;
     variant?: "primary" | "secondary" | "tertiary";
 };
-export default function CustomButton({ title, onPress, variant = "primary" }: Props) {
+export default function CustomButton({title, onPress, variant = "primary"}: Props){
     const styles = getStyles(variant);
 
-    return (
-        <TouchableOpacity style={styles.button} onPress={onPress}>
-            <Text style={styles.buttonTitle}> {title} </Text>
-        </TouchableOpacity>
+    return(
+            <TouchableOpacity style={styles.button} onPress={onPress}>
+                <Text style={styles.buttonTitle}> {title} </Text>
+            </TouchableOpacity>
     );
-}
+} 
 
-const getStyles = (variant: 'primary' | 'secondary' | 'tertiary') =>
+const getStyles = (variant: 'primary' | 'secondary' | 'tertiary') => 
     StyleSheet.create(
         {
-            button: {
+            button:{
                 borderColor: "#000",
                 borderRadius: 6,
-                padding: 12,
-                backgroundColor: variant === "primary" ? '#523954' :
-                    variant === "secondary" ? '#E3DBDB' :
-                        "#fff"
-            },
-            buttonTitle: {
-                color: variant === 'primary' ? '#ccc' :
-                    variant === 'secondary' ? '#000' :
+                padding: 12, 
+                marginBottom: 10,
+                backgroundColor: variant === "primary" ? '#2d0f2fff' : 
+                                    variant === "secondary" ? '#E3DBDB' :
+                                        "#fff"
+            }, 
+            buttonTitle:{
+                color: variant === 'primary'?'#ccc' : 
+                        variant === 'secondary'? '#000' :
                         '#656c78',
                 fontWeight: 'bold'
             }
